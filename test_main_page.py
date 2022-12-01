558ee90d56bcc90b69a2a351bf5350a9ed12f78b
anton <anbolob@belta.by>
2022-12-01 18:06:53 +0300

    2 Commit

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
