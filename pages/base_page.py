00d73327c31f5f695a2928438d7e09e1983e2cea
anton <anbolob@belta.by>
2022-12-05 18:01:32 +0300

    commit lesson 4.3 Улучшаем дизайн тестов

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,18 @@
-from selenium.common.exceptions import NoSuchElementException
+import math
+import time
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -15,4 +22,46 @@ class BasePage:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+        return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        #alert = self.browser.switch_to.alert
+        #alert.accept() работает без алерта
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def solve_quiz_and_get_code(self):
+        # WebDriverWait(self.browser, 3).until(EC.alert_is_present())
+        time.sleep(2)
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,24 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
+
+
+class MainPageLocators:
+    pass
 
 
 class LoginPageLocators:
     BUTTON_OPEN_FORM = (By.CSS_SELECTOR, '#registration_link')
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
+    NAME_BOOK = (By.CSS_SELECTOR, '.col-sm-6.product_main>h1')
+    MESSAGES_VIN = (By.CSS_SELECTOR, '#messages > div:nth-child(1)>div>strong')
+    PRICE_BOOK = (By.CSS_SELECTOR, 'div.col-sm-6.product_main>p.price_color')
+    PRICE_BOOK_IN_BASKET = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in strong")
