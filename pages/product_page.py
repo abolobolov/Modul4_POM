c8c2c33579c1c64538f0baa4b2180463b22c3b21
anton <anbolob@belta.by>
2022-12-05 18:01:45 +0300

    commit lesson 4.3 Улучшаем дизайн тестов

---
+++ pages/product_page.py
@@ -0,0 +1,44 @@
+import time
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def all_product_page(self):
+        self.should_not_be_success_message()
+        self.button_add_to_basket()
+        self.should_be_name_book()
+        self.should_be_price_book()
+        self.success_message_should_disappear()
+
+    def button_add_to_basket(self):
+        button_add_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        button_add_basket.click()
+        self.solve_quiz_and_get_code()
+
+    def should_be_name_book(self):
+        messages = self.browser.find_element(*ProductPageLocators.MESSAGES_VIN)
+        assert self.is_element_present(*ProductPageLocators.MESSAGES_VIN)
+        name_book = self.browser.find_element(*ProductPageLocators.NAME_BOOK)
+        time.sleep(2)
+        print(messages.text)
+        print(name_book.text)
+
+        assert messages.text == name_book.text, 'Название книги не совпадает с тем что добавлена в корзину'
+
+    def should_be_price_book(self):
+        price_book = self.browser.find_element(*ProductPageLocators.PRICE_BOOK)
+        price_book_in_basket = self.browser.find_element(*ProductPageLocators.PRICE_BOOK_IN_BASKET)
+        print(price_book.text)
+        print(price_book_in_basket.text)
+
+        assert price_book.text == price_book_in_basket.text, 'Цена на странице и в корзине отличаются'
+
+    def should_not_be_success_message(self):# показывает что сообщения об успешном добавлении товара в корзину нет пока товар не добавлен в корзину
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGES_VIN), \
+            "Присутствует сообщение об успешном добавлении товара в корзину, но товар не добавлен в корзину"
+
+    def success_message_should_disappear(self):# ждет пока сообщение об успешном добавлении товара исчезнет
+        assert self.is_disappeared(*ProductPageLocators.MESSAGES_VIN), "Сообщение об успешном добавлении товара в " \
+                                                                       "корзину не исчезает "
---
+++ test_product_page.py
@@ -0,0 +1,69 @@
+import pytest
+
+from .pages.product_page import ProductPage
+
+# link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+# link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
+
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+# @pytest.mark.parametrize('link', ["okay_link",
+#                                 pytest.param("bugged_link", marks=pytest.mark.xfail),
+#                                "okay_link"])
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{link}/?promo=offer{no}" for no in range(10)]
+link_2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207//?promo=offer0"
+
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.all_product_page()
+
+
+@pytest.mark.message('link_2')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_2)
+    page.open()
+    page.button_add_to_basket()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.message('link_2')
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link_2)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.message('link_2')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_2)
+    page.open()
+    page.button_add_to_basket()
+    page.success_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link3 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link3)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link4 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-big-u_93/"
+    page = ProductPage(browser, link4)
+    page.open()
+    page.go_to_login_page()
